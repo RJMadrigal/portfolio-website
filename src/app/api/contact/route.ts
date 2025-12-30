60f3f72a592b23a61ae5ef44135e846c709422b5
RJMadrigal <josuemm01@hotmail.com>
2025-12-30 09:31:08 -0600

    Fix - Resend API KEY Updated

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,10 +1,14 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
-
-const resend = new Resend(process.env.RESEND_API_KEY);
+const apiKey = process.env.RESEND_API_KEY;
 
 export async function POST(req: Request) {
   try {
+
+    if (!apiKey) return new Response("API key is not configured", { status: 500 });
+
+    const resend = new Resend(apiKey);
+
     const { name, email, message } = await req.json();
 
     if (!name || !email || !message) {
